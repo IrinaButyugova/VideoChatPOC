bf8943c4afee55d0185197467a37896072851fa6
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-11-25 15:31:41 +0300

    project design

---
+++ VideoChatPOC/VideoChatPOC/HubService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;
+
+namespace VideoChatPOC
+{
+    public class HubService : Hub
+    {
+    }
+}
---
+++ VideoChatPOC/VideoChatPOC/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VideoChatPOC
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<HubService>("/chat");
+            });
+        }
+    }
+}
