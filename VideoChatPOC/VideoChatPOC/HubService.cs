ca3954f93f2157a865c1769a9af1da01542fbde4
IrinaButyugova <irinabutyugova2403@gmail.com>
2021-11-29 11:38:16 +0300

    VideoChat

--- VideoChatPOC/VideoChatPOC/HubService.cs
+++ VideoChatPOC/VideoChatPOC/HubService.cs
@@ -6,5 +6,9 @@ namespace VideoChatPOC
 {
     public class HubService : Hub
     {
+        public async Task Send(string message)
+        {
+            await Clients.Others.SendAsync("Signaling", message);
+        }
     }
 }
